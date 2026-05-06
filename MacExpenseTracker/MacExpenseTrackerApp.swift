6393972826e27ece4ddae424b453aa50182499a0
Avinash <avinash592004@gmail.com>
2026-05-06 23:50:41 +0530

    Initial Commit

---
+++ MacExpenseTracker/MacExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacExpenseTrackerApp.swift
+//  MacExpenseTracker
+//
+//  Created by Avinash S on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MacExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacExpenseTrackerTests/MacExpenseTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacExpenseTrackerTests.swift
+//  MacExpenseTrackerTests
+//
+//  Created by Avinash S on 5/6/26.
+//
+
+import Testing
+@testable import MacExpenseTracker
+
+struct MacExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
