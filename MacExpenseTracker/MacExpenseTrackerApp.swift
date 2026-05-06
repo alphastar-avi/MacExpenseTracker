58ab9cbe039d99d0d5c1dfc04fae0a8f599f28b5
Avinash <avinash592004@gmail.com>
2026-05-07 00:42:31 +0530

    Initialize SwiftData ModelContainer in App entry

--- MacExpenseTracker/MacExpenseTrackerApp.swift
+++ MacExpenseTracker/MacExpenseTrackerApp.swift
@@ -1,17 +1,26 @@
-//
-//  MacExpenseTrackerApp.swift
-//  MacExpenseTracker
-//
-//  Created by Avinash S on 5/6/26.
-//
-
 import SwiftUI
+import SwiftData
 
 @main
 struct MacExpenseTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            User.self,
+            Transaction.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(sharedModelContainer)
     }
 }
